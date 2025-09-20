eaaa39e468b2c5804ee4da95cda9d11644c42606
nandou24 <nandou20@hotmail.com>
2025-09-20 00:16:43 -0500

    se inicia listado de atenciones

--- routes/Gestion/atencionEmpresaRoute.js
+++ routes/Gestion/atencionEmpresaRoute.js
@@ -1,7 +1,9 @@
-
 const { Router } = require("express");
 const { check } = require("express-validator");
-const { crearAtencionEmpresa } = require("../../controllers/Gestion/atencionEmpresaController");
+const {
+  crearAtencionEmpresa,
+  listarAtencionesVigentes,
+} = require("../../controllers/Gestion/atencionEmpresaController");
 const { validarCampos } = require("../../middlewares/validar-campo");
 const { validarJWT } = require("../../middlewares/validar-token");
 
@@ -9,15 +11,21 @@ const router = Router();
 
 // Registrar una nueva atención de empresa
 router.post(
-	"/",
-	[
-		validarJWT,
-		check("servicioTipo", "El tipo de servicio es obligatorio").not().isEmpty(),
-		check("fechaRegistro", "La fecha de registro es obligatoria").not().isEmpty(),
-		check("programaciones", "Debe haber al menos una programación").isArray({ min: 1 }),
-		validarCampos,
-	],
-	crearAtencionEmpresa
+  "/",
+  [
+    validarJWT,
+    check("servicioTipo", "El tipo de servicio es obligatorio").not().isEmpty(),
+    check("fechaRegistro", "La fecha de registro es obligatoria")
+      .not()
+      .isEmpty(),
+    check("programaciones", "Debe haber al menos una programación").isArray({
+      min: 1,
+    }),
+    validarCampos,
+  ],
+  crearAtencionEmpresa
 );
+
+router.get("/listar-vigentes", validarJWT, listarAtencionesVigentes);
 
 module.exports = router;
